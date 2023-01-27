2a7a2852a3552e39dcb3780873296c468ca4cc16
test <test@test.com>
2023-01-27 18:41:29 +0900

    인터셉터 페이지 권한 추가

--- src/main/java/com/memo/config/WebMvcConfig.java
+++ src/main/java/com/memo/config/WebMvcConfig.java
@@ -1,14 +1,20 @@
 package com.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.memo.common.FileManagerService;
+import com.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
 	@Override
 	public void addResourceHandlers(
 			ResourceHandlerRegistry registry) {
@@ -17,7 +23,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		.addResourceHandler("/images/**")   // 웹 이미지 주소 http://localhost:8080/images/aaaa_16205468768/sun.png
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH); // 실제 파일 위치
 	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")        //     /** 아래 디렉토리까지 확인
+		.excludePathPatterns("/favicon.ico", "/error", "/static/**", "/user/sign_out");
+	}
 }
+
+
+
+
+
 
 
 
